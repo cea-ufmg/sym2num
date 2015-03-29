85bf8dec3da52f8479643ed4bbc2af62e14257cd
Dimas Abreu Dutra <dimasadutra@gmail.com>
2015-03-29 18:31:28 -0300

    setup: Bumping version to next development release.

--- setup.py
+++ setup.py
@@ -2,10 +2,27 @@
 
 from setuptools import setup, find_packages
 
+DESCRIPTION = '''\
+Generates python code from sympy functions and models.
+'''
+
+CLASSIFIERS = '''\
+Intended Audience :: Developers
+Intended Audience :: Science/Research
+License :: OSI Approved
+Operating System :: MacOS
+Operating System :: Microsoft :: Windows
+Operating System :: POSIX
+Operating System :: Unix
+Programming Language :: Python
+Programming Language :: Python :: 3
+Programming Language :: Python :: 3 :: Only
+Topic :: Scientific/Engineering
+Topic :: Software Development'''
 
 setup(
     name="sym2num",
-    version="0.1.dev1",
+    version="0.1.dev2",
     packages=find_packages(),
     install_requires=["attrdict", "numpy", "pystache", "sympy"],
     test_requires=["pytest"],
@@ -14,6 +31,9 @@ setup(
     author="Dimas Abreu Dutra",
     author_email="dimasadutra@gmail.com",
     description="Sympy to numpy code generator.",
+    long_description=DESCRIPTION,
+    platforms=["Windows", "Linux", "Solaris", "Mac OS-X", "Unix"],
+    classifiers=[CLASSIFIERS.split('\n')],
     license="MIT",
     url="http://github.com/dimasad/sym2num",
 )
